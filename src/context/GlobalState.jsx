dc2c8d064ba6c55f567394d2de9e767e35897a9f
Aram Eli <Aram221b@gmail.com>
2022-06-29 03:42:50 +0100

    Creaing the Transactionadd and app Reducer and globalstate inclduing the basic logic and However from the initialStatic info hhas been deleted

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -4,12 +4,7 @@ import AppReducer from './appReducer';
 
 // Initial state
 const initialState = {
-   transactions: [
-    {id: 1, text: "Flower", amount: -20 },
-    {id: 2, text: "Salary", amount: 300 },
-    {id: 3, text: "Book", amount:   -10 },
-    {id: 4, text: "Camera", amount: 150 }
-   ]
+   transactions: []
 };
 
 // CreateContext
@@ -19,7 +14,22 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({children}) => {
    const [state, dispatch] = useReducer(AppReducer, initialState);
 
-   return (<GlobalContext.Provider value={{transactions: state.transactions}}>
+   // Actions caused to my reducer
+   function deleteTransaction (id) {
+      dispatch({
+         type: "DELETE_TRANSACTION",
+         payload: id
+      });
+   };
+
+   function addTransaction (transaction) {
+      dispatch({
+         type: "ADD_TRANSACTION",
+         payload: transaction
+      });
+   };
+
+   return (<GlobalContext.Provider value={{transactions: state.transactions, deleteTransaction, addTransaction}}>
      {children}
    </GlobalContext.Provider>)
 };
